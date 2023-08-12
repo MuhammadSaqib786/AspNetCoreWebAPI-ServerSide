b7e9c8718490746e2f6589be092f517f3b2b448e
MuhammadSaqib786 <saqisaqi.sk@gmail.com>
2023-08-12 11:55:13 +0500

    created productscontroller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCoreWebAPI_Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebAPI_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context) {
+
+            _context = context;
+
+        }
+
+    }
+}
