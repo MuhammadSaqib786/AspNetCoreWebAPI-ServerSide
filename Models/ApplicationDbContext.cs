55e4348afef3154ff2847f09dd661ac29f849cc2
MuhammadSaqib786 <saqisaqi.sk@gmail.com>
2023-08-12 11:48:15 +0500

    migrations done

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -4,6 +4,10 @@ namespace AspNetCoreWebAPI_Project.Models
 {
     public class ApplicationDbContext: DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
 
 
     }
